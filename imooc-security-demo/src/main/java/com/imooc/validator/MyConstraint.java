728e611b02cda27a36b6caabea2bae89580fff83
zhouqi <274617143@qq.com>
2018-05-03 16:34:32 +0800

    自定义注解

--- imooc-security-demo/src/main/java/com/imooc/validator/MyConstraint.java
+++ imooc-security-demo/src/main/java/com/imooc/validator/MyConstraint.java
@@ -7,11 +7,18 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * 自定义注解
+ */
+
+// 指定可以标注的范围
 @Target({ElementType.METHOD, ElementType.FIELD})
 @Retention(RetentionPolicy.RUNTIME)
+// 指定当前注解需要什么类去校验
 @Constraint(validatedBy = MyConstraintValidator.class)
 public @interface MyConstraint {
-	
+	// 下面三个属性必须写
+
 	String message();
 
 	Class<?>[] groups() default { };
--- imooc-security-demo/src/main/java/com/imooc/validator/MyConstraintValidator.java
+++ imooc-security-demo/src/main/java/com/imooc/validator/MyConstraintValidator.java
@@ -6,16 +6,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
-public class MyConstraintValidator implements ConstraintValidator<MyConstraint, Object> {
+/**
+ * 自定义注解的实现
+ */
+
+public class MyConstraintValidator implements ConstraintValidator<MyConstraint, Object> { // Object 是指注解可以放在哪种类型上
 
 	@Autowired
 	private HelloService helloService;
-	
+
+	// 初始化
 	@Override
 	public void initialize(MyConstraint constraintAnnotation) {
 		System.out.println("my validator init");
 	}
 
+	// 校验 成功返回true  失败返回false
 	@Override
 	public boolean isValid(Object value, ConstraintValidatorContext context) {
 		helloService.greeting("tom");
